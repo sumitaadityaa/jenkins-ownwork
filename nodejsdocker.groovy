1e51ad40cfca24a591f5493637cd5359976e299e
sumitaadityaa <sumitaadityaa@gmail.com>
2021-03-24 02:10:06 +0530

    complete dsl job

--- nodejsdocker.groovy
+++ nodejsdocker.groovy
@@ -5,4 +5,23 @@ job("gitNdocker-nodeJS-dsl") {
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
+    triggers {
+          scm('H/5 * * * *')
+      }
+      wrappers {
+          nodejs('nodejs-agent') // this is the name of the NodeJS installation in
+                           // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+      }
+      steps {
+          dockerBuildAndPublish {
+              repositoryName('sumitaadityaa/docker-repo')
+              tag('${GIT_REVISION,length=9}')
+              registryCredentials('docker credential')
+              forcePull(false)
+              forceTag(false)
+              createFingerprints(false)
+              skipDecorate()
+          }
+      }
+  }
 }
