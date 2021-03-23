e1f4fc1831d658a28a3d7e04f4c9be139495bfc9
sumitaadityaa <sumitaadityaa@gmail.com>
2021-03-24 01:50:52 +0530

    main branch updated

--- nodejsdocker.groovy
+++ nodejsdocker.groovy
@@ -1,6 +1,6 @@
 job("gitNdocker-nodeJS-dsl") {
   scm{
-   git('https://github.com/sumitaadityaa/docker-demo.git','master') {  node -> // is hudson.plugins.git.GitSCM
+   git('https://github.com/sumitaadityaa/docker-demo.git','main') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
